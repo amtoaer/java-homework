7977c6c07d69e2a1897d4a463696f059638d509e
amtoaer <amtoaer@outlook.com>
2020-09-28 10:56:22 +0800

    水题1

---
+++ 交换两个数.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+        System.out.println(b);
+        System.out.println(a);
+        cin.close();
+    }
+}
